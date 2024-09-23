f9482945b2e4eac36205e183a932c2bbc0c1606c
조하은 <jhe93@naver.com>
2024-09-23 15:57:50 +0900

    fix(practice): Header 컴포넌트 분리
    Header 컴포넌트를 App.jsx 파일에서 분리하였습니다.

--- practice/01-starting-project/src/App.jsx
+++ practice/01-starting-project/src/App.jsx
@@ -1,27 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/Components.png';
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
+import Header from'./components/Header.jsx';
 
 function CoreConcept({image, title, description}) {
   return <li>
