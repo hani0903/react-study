0b8f670a040ffb0865af1e4ab181e291f1e33550
조하은 <jhe93@naver.com>
2024-09-23 15:11:33 +0900

    fix(practice): props 적용
    props를 적용하여 컴포넌트들을 출력하도록 코드를 수정했습니다.

--- practice/01-starting-project/src/App.jsx
+++ practice/01-starting-project/src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/Components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,11 +23,37 @@ function Header() {
   );
 }
 
+function CoreConcept({image, title, description}) {
+  return <li>
+    <img src={image} alt={title}/>
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept 
+              title={CORE_CONCEPTS[1].title} 
+              description={CORE_CONCEPTS[1].description} 
+              image={CORE_CONCEPTS[1].image}/>
+            <CoreConcept 
+              title={CORE_CONCEPTS[2].title} 
+              description={CORE_CONCEPTS[2].description} 
+              image={CORE_CONCEPTS[2].image}/>
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title} 
+              description={CORE_CONCEPTS[3].description} 
+              image={CORE_CONCEPTS[3].image}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
