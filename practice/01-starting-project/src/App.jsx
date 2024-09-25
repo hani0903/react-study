230410af38298387f4c7d7e266b74f4302ea5e9b
조하은 <jhe93@naver.com>
2024-09-25 15:54:13 +0900

    [54강]: State에 따라 배열 값 갖고 오기
    State의 값에 따라 객체에서 값을 읽어온 후 출력하였습니다.

--- practice/01-starting-project/src/App.jsx
+++ practice/01-starting-project/src/App.jsx
@@ -7,10 +7,11 @@ import CoreConcept from'./components/CoreConcept.jsx';
 import TabButton from'./components/TabButton.jsx';
 
 import { useState } from 'react';
+import { EXAMPLES } from './data.js';
 
 function App() {
 
-  let [selectedTopic, setSelectedTopic] = useState('please click a button');
+  let [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -47,7 +48,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
         <h2>Time to get started!</h2>
       </main>
