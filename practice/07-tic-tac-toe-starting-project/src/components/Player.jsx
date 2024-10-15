3765f925a37673ce68719b5812d1f4ba1113dfbd
조하은 <jhe93@naver.com>
2024-10-15 23:14:35 +0900

    [78강]: 양방향 바인딩
    input 태그를 통해 입력받은 값으로 다시 바인딩하는 방법을 공부했습니다.

--- practice/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ practice/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,30 +1,32 @@
 import {useState} from 'react';
 
+export default function Player({initialName, symbol}) {
 
-
-export default function Player({name, symbol}) {
-
-    const [ isEditing, setIsEditing ] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-
-        setIsEditing(true);
-        return;
+        setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if(isEditing) {
-        playerName = <input type='text' required/>
+        editablePlayerName = <input type='text' required value={playerName} onChange={handleChange}/>
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
